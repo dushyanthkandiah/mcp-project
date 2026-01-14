d428f0f3be6e3cacb69536c65c44cc187677e2ee
Dushyanth Kandiah <dushyanthkandiah@gmail.com>
2026-01-14 19:49:17 +0530

    aasd

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace MCPApi.Controllers;
 
-// [Authorize]
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class CustomerController : ControllerBase
